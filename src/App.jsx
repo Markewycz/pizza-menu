7d4bcac8274d82d822409aeb4aa36c10cdee99b2
Markewycz <gracjanmarkiewicz00@gmail.com>
2023-08-21 12:30:37 +0200

    feat: finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,51 @@
+import foccacia from "./assets/focaccia.jpg";
+import funghi from "./assets/funghi.jpg";
+import margherita from "./assets/margherita.jpg";
+import prosciutto from "./assets/prosciutto.jpg";
+import salamino from "./assets/salamino.jpg";
+import spinaci from "./assets/spinaci.jpg";
+
 const pizzaData = [
   {
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "src/assets/focaccia.jpg",
+    photoName: foccacia,
     soldOut: false,
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "src/assets/margherita.jpg",
+    photoName: margherita,
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "src/assets/spinaci.jpg",
+    photoName: spinaci,
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "src/assets/funghi.jpg",
+    photoName: funghi,
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "src/assets/salamino.jpg",
+    photoName: salamino,
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "src/assets/prosciutto.jpg",
+    photoName: prosciutto,
     soldOut: false,
   },
 ];
